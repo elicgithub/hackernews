6cd2deacb3aea1d1ef1931c327c213d5ade2d5f4
elicgithub <51258813+elicgithub@users.noreply.github.com>
2019-12-21 16:29:16 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -68,30 +68,6 @@ class App extends Component {
           pattern={searchTerm}
           onDismiss={this.onDismiss}
           />
-        {/* {<form>
-          <input
-          type="text"
-          value={searchTerm}
-          onChange={this.onSearchChange}/>
-        </form> } */}
-        {/* {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-              Dismiss
-              </button>
-            </span>
-          </div>
-        )} */}
       </div>
     );
   }
